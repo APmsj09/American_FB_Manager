c71ef18bfa6ca6fb0f0e678983569124bb949bcf
Andrew Phelps <apmsj09@gmail.com>
2025-09-15 11:04:01 +0900

    Update Coach.js

--- src/models/Coach.js
+++ src/models/Coach.js
@@ -1,20 +1,13 @@
-// src/models/Player.js
-export default class Player {
+// src/models/Coach.js
+export default class Coach {
     constructor(data = {}) {
         this.id = data.id || crypto.randomUUID();
         this.name = data.name || '';
-        this.position = data.position || 'N/A';
-        this.age = data.age || 21;
+        this.age = data.age || 35;
         this.teamId = data.teamId || null;
-        this.league = data.league || '';
-        this.attributes = data.attributes || {};
     }
 
-    getOverallRating() {
-        if (!this.attributes || Object.keys(this.attributes).length === 0) {
-            return 50;
-        }
-        const attrs = Object.values(this.attributes);
-        return Math.round(attrs.reduce((sum, val) => sum + val, 0) / attrs.length);
+    assignTeam(teamId) {
+        this.teamId = teamId;
     }
 }
